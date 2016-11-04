f12c52990dfed888eb3152a752fc89cb685d616a
ArnaudParant <ard.parant@gmail.com>
2016-11-04 22:28:14 +0100

    Add docker install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get purge "lxc-docker*"
+sudo apt-get purge "docker.io*"
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo sh -c 'echo "deb https://apt.dockerproject.org/repo debian-wheezy main" >> /etc/apt/sources.list.d/docker.list'
+sudo apt-get update
+sudo apt-get install docker-engine
+sudo service docker start
